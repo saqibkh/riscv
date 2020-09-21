b8e3cb0eb60c31a5d6a23d5864ccf018034cce21
saqibkh <saqibkhan@utexas.edu>
2020-09-21 13:17:36 -0500

    YACCA signature calculations

--- scripts/Control_Flow_Error/yacca.py
+++ scripts/Control_Flow_Error/yacca.py
@@ -10,6 +10,7 @@ import subprocess
 import utils
 from os import path
 
+
 class YACCA:
     def __init__(self, i_map):
         self.original_map = i_map
@@ -39,5 +40,82 @@ class YACCA:
         self.new_asm_file = self.original_map.file_asm
 
         utils.generate_instruction_mapping(self)
-        self.process_CFCSS_blocks()
-        self.generate_CFCSS_file_updated()
+
+        # 1) Generate compile time signatures (B_i) for each block
+        random.seed(a=1, version=2)
+        self.compile_time_sig = random.sample(range(1, len(self.original_map.blocks) * 2),
+                                              len(self.original_map.blocks))
+
+        # 2) Generate the 2nd compile-time variable named Previous
+        self.generate_Previous()
+
+        # 3) Generate M1 for blocks with 2 predecessors
+        self.generate_M1()
+
+        # 4) Generate M2 which is the fourth compile-time variable
+        self.generate_M2()
+
+        self.generate_YACCA_file_updated()
+
+        return
+
+    ''' Beginning of class function definitions '''
+    def generate_Previous(self):
+        for i in range(len(self.original_map.blocks)):
+            # Take care of case where there is no incoming block (i.e. Main())
+            if len(self.original_map.blocks[i].previous_block_id) == 0:
+                self.previous.append(0)
+
+            # Block with only 1 incoming block
+            elif len(self.original_map.blocks[i].previous_block_id) == 1:
+                self.previous.append(self.compile_time_sig[self.original_map.blocks[i].previous_block_id[0]])
+
+            # Block with 2 incoming blocks
+            else:
+                sig_1 = self.compile_time_sig[self.original_map.blocks[i].previous_block_id[0]]
+                sig_2 = self.compile_time_sig[self.original_map.blocks[i].previous_block_id[1]]
+                self.previous.append(sig_1 * sig_2)
+
+    def generate_M1(self):
+        for i in range(len(self.original_map.blocks)):
+            if len(self.original_map.blocks[i].previous_block_id) == 2:
+                sig_1 = self.compile_time_sig[self.original_map.blocks[i].previous_block_id[0]]
+                sig_2 = self.compile_time_sig[self.original_map.blocks[i].previous_block_id[1]]
+                M1 = ~(sig_1 ^ sig_2)
+                # Flip the sign if M1 is negative
+                if(M1 < 0):
+                    M1 *= -1
+                self.M1[i] = M1
+
+
+    def generate_M2(self):
+        for i in range(len(self.original_map.blocks)):
+            # For blocks that are end blocks i.e. has no outgoing blocks, we don't need to get M2
+            if len(self.original_map.blocks[i].next_block_id) == 0:
+                continue
+
+            if len(self.original_map.blocks[i].previous_block_id) == 0:
+                # M2 = 1 Exclusive OR B_i
+                B_i = self.compile_time_sig[i]
+                self.M2[i] = 1 ^ B_i
+
+            elif len(self.original_map.blocks[i].previous_block_id) == 1:
+                # M2 = B_pred1 Exclusive OR B_i
+                B_pred1 = self.compile_time_sig[self.original_map.blocks[i].previous_block_id[0]]
+                B_i = self.compile_time_sig[i]
+                self.M2[i] = B_pred1 ^ B_i
+
+            elif len(self.original_map.blocks[i].previous_block_id) == 2:
+                # M2 = (B_pred1 AND M1_i) Exclusive OR B_i
+                B_pred1 = self.compile_time_sig[self.original_map.blocks[i].previous_block_id[0]]
+                M1_i = self.M1[i]
+                B_i = self.compile_time_sig[i]
+                self.M2[i] = (B_pred1 & M1_i) ^ B_i
+
+
+
+
+    def generate_YACCA_file_updated(self):
+        return
+
+    ''' End of class definitions '''
