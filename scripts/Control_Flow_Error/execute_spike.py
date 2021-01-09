1fdc05d1f7abe2cb352c73dd7ec037b1b5c8a8ba
saqibkh <saqibkhan@utexas.edu>
2021-01-09 14:28:35 -0600

    Updates
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/Control_Flow_Error/execute_spike.py
+++ scripts/Control_Flow_Error/execute_spike.py
@@ -18,13 +18,8 @@ import registers
 def get_registers_values_at_address(i_executable_file, i_address):
     i_reg_list = []
 
-    cmd = '/opt/riscv/bin/spike -d /opt/riscv/toolchain/riscv64-unknown-linux-gnu/bin/pk ' + i_executable_file
-    child = pexpect.spawn(cmd)
-    child.expect([':', pexpect.EOF])
-
-    # Proceed to the desired address
-    child.sendline("until pc 0 " + i_address)
-    child.expect(['\r\n', pexpect.EOF])
+    # child is a process that triggers the spike simulator
+    child = execute_spike_address(i_executable_file, i_address)
 
     for i in range(len(registers.regular_registers)):
         # Get the register values
@@ -44,3 +39,60 @@ def get_registers_values_at_address(i_executable_file, i_address):
 
     sys.stdout.flush()
     return i_reg_list
+
+
+def get_register_at_address(i_executable_file, i_address, i_register):
+    child = execute_spike_address(i_executable_file, i_address)
+
+    cmd = "reg 0 " + i_register
+    child.sendline(cmd)
+    child.expect([cmd + '\r\n', pexpect.EOF])
+    i_reg_value = ((child.readline()).decode("utf-8")).split("\r\n")[0]
+    return i_reg_value
+
+
+def get_floating_point_register_at_address(i_executable_file, i_address, i_floating_register):
+    child = execute_spike_address(i_executable_file, i_address)
+    cmd = "fregs 0 " + i_floating_register
+    child.sendline(cmd)
+    child.expect([cmd + '\r\n', pexpect.EOF])
+    i_reg_value = ((child.readline()).decode("utf-8")).split("\r\n")[0]
+    return i_reg_value
+
+
+# Get the memory data after reaching the specified address
+def get_memory_data_at_address(i_executable_file, i_address, i_mem_location):
+    child = execute_spike_address(i_executable_file, i_address)
+    cmd = "mem " + i_mem_location
+    child.sendline(cmd)
+    child.expect([cmd + '\r\n', pexpect.EOF])
+    i_mem_value = ((child.readline()).decode("utf-8")).split("\r\n")[0]
+    return i_mem_value
+
+
+# Returns the data stored in the memory location
+def get_memory_datas(i_executable_file, i_mem_location):
+    child = execute_spike(i_executable_file)
+    cmd = "mem " + i_mem_location
+    child.sendline(cmd)
+    child.expect([cmd + '\r\n', pexpect.EOF])
+    i_mem_value = ((child.readline()).decode("utf-8")).split("\r\n")[0]
+    return i_mem_value
+
+
+def execute_spike_address(i_executable_file, i_address):
+    cmd = '/opt/riscv/bin/spike -d /opt/riscv/toolchain/riscv64-unknown-linux-gnu/bin/pk ' + i_executable_file
+    child = pexpect.spawn(cmd)
+    child.expect([':', pexpect.EOF])
+
+    # Proceed to the desired address
+    child.sendline("until pc 0 " + i_address)
+    child.expect(['\r\n', pexpect.EOF])
+    return child
+
+
+def execute_spike(i_executable_file):
+    cmd = '/opt/riscv/bin/spike -d /opt/riscv/toolchain/riscv64-unknown-linux-gnu/bin/pk ' + i_executable_file
+    child = pexpect.spawn(cmd)
+    child.expect([':', pexpect.EOF])
+    return child
