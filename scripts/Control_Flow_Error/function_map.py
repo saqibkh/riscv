672f0359d814ba3724d1b51c237af8a1d0b2de68
saqibkh <saqibkhan@utexas.edu>
2021-01-15 11:10:02 -0600

    Updates to FunctionalMap
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/Control_Flow_Error/function_map.py
+++ scripts/Control_Flow_Error/function_map.py
@@ -16,10 +16,17 @@ from os import path
 ''' Start of class definitions'''
 
 
+# TODO: We are currently assuming that each function is called only once. Thus the initial and final values
+#       of a register are all static. This may not be true if a function is called multiple times with different
+#       input values.
 class Function:
     def __init__(self, i_name):
         # Name of the function such as main or countSetBits
         self.name = i_name
+
+        # This var holds the name of the function which calls this particular function.
+        # Main will have no parent function, but all others must have a parent/calling function.
+        self.parent_function = None
 
         # List of functions that are called from within this particular function.
         # For example main calling printf and countSetBits
@@ -45,6 +52,9 @@ class Function:
         # List of registers that are not modified during this function (i.e. remaining subset of registers_used
         self.registers_unmodified = []
 
+        # This is a list of registers that have different values at the start and end of a function.
+        self.registers_different_values = []
+
 
 class FunctionMap:
     ##
@@ -56,11 +66,32 @@ class FunctionMap:
         self.functions = []
 
         self.get_functions(i_asm)
+        self.get_parent_function()
         self.get_instructions(i_util_functions)
         self.process_registers(i_C_File)
 
-    def process_registers(self, i_C_File):
+    # Returns the ID of the function whose name matches with "i_func_name". Otherwise return None.
+    def get_function_id_matching_name(self, i_func_name):
+        for i in range(len(self.functions)):
+            if self.functions[i].name == i_func_name:
+                return i
+        return None
 
+    def get_parent_function(self):
+        for i in range(len(self.functions)):
+            for j in range(len(self.functions[i].list_sub_functions)):
+                i_sub_function = self.functions[i].list_sub_functions[j]
+                i_func_id = self.get_function_id_matching_name(i_sub_function)
+                if i_func_id is not None:
+                    self.functions[i_func_id].parent_function = self.functions[i].name
+
+    # This function does the following things
+    # 1. Gets the starting and ending addresses of each function
+    # 2. Gets the register values at the start and end of each function
+    # 3. Get a list of registers that are modified
+    # 4. Fill the list of registers that weren't modified in the function
+    # 5. Get a list of registers that are different from start and end along with their different values
+    def process_registers(self, i_C_File):
         # Get a value of all registers at the start and end of each function
         for i in range(len(self.functions)):
             self.functions[i].initial_register_values = execute_spike.get_registers_values_at_address(
@@ -87,6 +118,17 @@ class FunctionMap:
                 else:
                     self.functions[i].registers_unmodified.append(i_reg)
 
+        # 5. Get a list of registers that are different from start and end along with their different values
+        for i in range(len(self.functions)):
+            for j in range(len(self.functions[i].initial_register_values)):
+                reg = self.functions[i].initial_register_values[j][0]
+                initial_value = self.functions[i].initial_register_values[j][1]
+                final_value = self.functions[i].final_register_values[j][1]
+                if initial_value != final_value:
+                    self.functions[i].registers_different_values.append([reg, initial_value, final_value])
+
+    # This function gets the function names from the assembly file.
+    # It also gets a list of sub-function(functions that are called from a parent function)
     def get_functions(self, i_asm):
         i_found_function = False
         for i in range(len(i_asm)):
@@ -131,6 +173,8 @@ class FunctionMap:
             # Update the entire list
             self.functions[i].list_sub_functions = i_new_list
 
+    # This function gets the starting and ending addresses of each function along with a list of
+    # instructions that makes up a function.
     def get_instructions(self, i_util_functions):
         # TODO: It is possible that there are more than 1 possible return calls within each function (besides the one
         #       at the very end of instruction stream). Currently we aren't handling this case.
