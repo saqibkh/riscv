5d66ede14e91c3b6334fe27b541b2e8afac4b8f2
saqibkh <saqibkhan@utexas.edu>
2021-02-12 10:11:41 -0600

    Add CFE TRIAL2_1
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/Control_Flow_Error/CFE_MAIN.py
+++ scripts/Control_Flow_Error/CFE_MAIN.py
@@ -36,6 +36,7 @@ import ecca
 import rscfc
 import trial1
 import trial2
+import trial2_1
 import trial3
 import instructions
 import sim_logging
@@ -54,7 +55,7 @@ def checkFileExists(i_filename):
 
 
 def main(argv):
-    l_enable_extras = False
+    l_enable_extras = True
     simlog = sim_logging.SIMLOG()
 
     if len(sys.argv) == 1:
@@ -88,6 +89,50 @@ def main(argv):
     ######   Start generating the Control Flow Error Detection ######
     ######                                                     ######
     #################################################################
+
+#####################################################################################################################
+    # Generate TRIAL2_1
+    simlog.info("------------------------------------------------------------------------------------------------")
+    simlog.info("Start processing TRIAL2_1")
+    map = utils.ControlFlowMapRevised(utils.readfile(file_s), utils.readfile(file_objdump), simlog=simlog)
+    i_trial2_1 = trial2_1.TRIAL2_1(map)
+    trial2_1_file = argv[0].rsplit('.')[0] + '_trial2_1.s'
+    trial2_1_file_objdump = argv[0].rsplit('.')[0] + '_trial2_1.objdump'
+    with open(trial2_1_file, 'w') as filehandle:
+        for listitem in i_trial2_1.new_asm_file:
+            filehandle.write('%s\n' % listitem)
+    compileUtil.compile_s(trial2_1_file)  # Compile the newly created assembly file to generate a static binary
+
+    ## Re-read the <test>_intermediate_trial2_1 objdump and .s file and form the Control Flow Graph again
+    update_file_required = True
+    # loop until we get the same signature values
+    while update_file_required:
+        trial2_1_s_intermediate_file = utils.readfile(trial2_1_file)
+        trial2_1_obj_intermediate_file = utils.readfile(trial2_1_file.split(".s")[0] + ".objdump")
+        trial2__1s_intermediate_file, trial2_obj_intermediate_file = i_trial2_1.remove_signature_checking(
+            trial2_1_s_intermediate_file, trial2_1_obj_intermediate_file)
+        map_new = utils.ControlFlowMapRevised(trial2_1_s_intermediate_file, trial2_1_obj_intermediate_file)
+        map_new = i_trial2_1.update_opcodes(map, map_new)
+        i_trial2_1_new = trial2_1.TRIAL2_1(map_new, i_generate_signature_only=True)
+        # We have old and new signatures in i_trial2_1 and i_trial2_1_new respectively.
+        update_file_required = trial2_1.update_signature(i_trial2_1, i_trial2_1_new, trial2_1_file)
+        compileUtil.compile_s(trial2_1_file)  # Compile the newly created assembly file to generate a static binary
+        i_trial2_1 = i_trial2_1_new
+        map = map_new
+
+    # My storing signature in memory (compared to loading via instructions) is we save a lot of instructions
+    trial2_1.store_signature_in_memory(trial2_1_file)
+
+    # Get the memory_size of the original and modified file and find it's diff
+    new_map = utils.ControlFlowMapRevised(utils.readfile(trial2_1_file), utils.readfile(trial2_1_file_objdump),
+                                          simlog=simlog)
+    utils.get_memory_size_info(map, new_map, simlog=simlog)
+
+    del i_trial2_1, i_trial2_1_new, new_map, map, map_new, trial2_1_s_intermediate_file, trial2_1_obj_intermediate_file
+    del trial2_1_file, trial2_1_file_objdump, update_file_required
+    simlog.info("Finished processing TRIAL2_1")
+    simlog.info("------------------------------------------------------------------------------------------------\n\n")
+#####################################################################################################################
 
 #####################################################################################################################
     # Generate CFCSS (Control Flow Checking by Software Signature)
