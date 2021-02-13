979bc36080f262e68cddffaedbdde2658bbbd939
saqibkh <saqibkhan@utexas.edu>
2021-02-13 11:40:04 -0600

    Updates
    Signed-off-by: saqibkh <saqibkhan@utexas.edu>

--- scripts/utils/instructions.py
+++ scripts/utils/instructions.py
@@ -49,19 +49,50 @@ extra_instructions = csr_instructions + change_level + mmu_instruction + other_i
 ########################################################################################################
 
 ########################################################################################################
-floating_point_instructions = ['fmv.w.x']
-floating_point_arithmetic = ['fcvt.d.lu', 'fdiv.d', 'fdiv.s', 'fcvt.s.l', 'fcvt.s.w', 'fmul.s', 'fmul.d',
-                             'fcvt.l.s', 'fsub.s', 'fadd.s', 'fcvt.d.s', 'fmv.x.d', 'fcvt.s.d']
-floating_point_comparision = ['flt.s', 'fle.s']
-floating_point_load = ['fld', 'flw']
-floating_point_store = ['fsd', 'fsw']
+floating_point_move = ['fmv.h.x', 'fmv.s.x', 'fmv.d.x', 'fmv.q.x',
+                       'fmv.x.h', 'fmv.x.s', 'fmv.x.d', 'fmv.x.q']
+
+floating_point_convert = ['fcvt.h.w', 'fcvt.s.w', 'fcvt.d.w', 'fcvt.q.w',
+                          'fcvt.h.l', 'fcvt.s.l', 'fcvt.d.l', 'fcvt.q.l',
+                          'fcvt.h.t', 'fcvt.s.t', 'fcvt.d.t', 'fcvt.q.t',
+                          'fcvt.h.wu', 'fcvt.s.wu', 'fcvt.d.wu', 'fcvt.q.wu',
+                          'fcvt.h.lu', 'fcvt.s.lu', 'fcvt.d.lu', 'fcvt.q.lu',
+                          'fcvt.h.tu', 'fcvt.s.tu', 'fcvt.d.tu', 'fcvt.q.tu',
+                          'fcvt.w.h', 'fcvt.w.s', 'fcvt.w.d', 'fcvt.w.q',
+                          'fcvt.l.h', 'fcvt.l.s', 'fcvt.l.d', 'fcvt.l.q',
+                          'fcvt.t.h', 'fcvt.t.s', 'fcvt.t.d', 'fcvt.t.q',
+                          'fcvt.wu.h', 'fcvt.wu.s', 'fcvt.wu.d', 'fcvt.wu.q',
+                          'fcvt.lu.h', 'fcvt.lu.s', 'fcvt.lu.d', 'fcvt.lu.q',
+                          'fcvt.tu.h', 'fcvt.tu.s', 'fcvt.tu.d', 'fcvt.tu.q',
+                          'fcvt.d.s', 'fcvt.s.d']
+floating_point_arithmetic = ['fadd.s', 'fadd.d', 'fadd.q',
+                             'fsub.s', 'fsub.d', 'fsub.q',
+                             'fmul.s', 'fmul.d', 'fmul.q',
+                             'fdiv.s', 'fdiv.d', 'fdiv.q',
+                             'fsqrt.s', 'fsqrt.d', 'fsqrt.q',
+                             'fmadd.s', 'fmadd.d', 'fmadd.q',
+                             'fmsub.s', 'fmsub.d', 'fmsub.q',
+                             'fnmadd.s', 'fnmadd.d', 'fnmadd.q',
+                             'fnmsub.s', 'fnmsub.d', 'fnmsub.q']
+floating_point_inject_sign = ['fsgnj.s', 'fsgnj.d', 'fsgnj.q',
+                              'fsgnjn.s', 'fsgnjn.d', 'fsgnjn.q',
+                              'fsgnjx.s', 'fsgnjx.d', 'fsgnjx.q']
+floating_point_min_max = ['fmin.s', 'fmin.d', 'fmin.q',
+                          'fmax.s', 'fmax.d', 'fmax.q']
+floating_point_compare = ['feq.s', 'feq.d', 'feq.q',
+                          'flt.s', 'flt.d', 'flt.q',
+                          'fle.s', 'fle.d', 'fle.q']
+all_floating_point_arithmetic = floating_point_move + floating_point_convert + floating_point_arithmetic + \
+                                floating_point_inject_sign + floating_point_min_max + floating_point_compare
+
+floating_point_load = ['flw', 'fld', 'flq']
+floating_point_store = ['fsw', 'fsd', 'fsq']
 ########################################################################################################
 
-
 all_instructions = all_arithmetic_instructions + branch_instructions + load_store_instructions +\
-                   extra_instructions + floating_point_instructions
+                   extra_instructions + all_floating_point_arithmetic
 
 reg_modified_instructions = all_arithmetic_instructions + load_instructions + branch_unconditional_instructions + \
-                            floating_point_load + floating_point_arithmetic + floating_point_comparision
+                            all_floating_point_arithmetic + floating_point_load
 reg_unmodified_instructions = store_instructions + branch_conditional_instructions + other_instructions + \
                               floating_point_store
